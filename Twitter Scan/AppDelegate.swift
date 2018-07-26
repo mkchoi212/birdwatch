6212c8005e20adf9923b57ce85a4e7adbf8f1e93
Mike JS. Choi <mkchoi212@icloud.com>
2018-07-26 15:52:53 +0900

    Initial Commit

---
+++ Twitter Scan/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Twitter Scan
+//
+//  Created by Mike Choi on 7/26/18.
+//  Copyright © 2018 Mike JS. Choi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Twitter Scan/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Twitter Scan
+//
+//  Created by Mike Choi on 7/26/18.
+//  Copyright © 2018 Mike JS. Choi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
